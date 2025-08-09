bb8fd6ec378bc5e37ae56d96c0c141e900d6533b
habiboll-Ahatashbahar <habibollahatashbahar@gmail.com>
2025-08-10 00:02:42 +0330

    Second Chapter

---
+++ JS-2/javascript-2.js
@@ -0,0 +1,154 @@
+// ---------------- First -----------------
+// Arithmetic Operators
+// let x = 5;
+// let y = 3;
+
+// // +
+// console.log(x + y);
+
+// // -
+// console.log(x - y);
+
+// // *
+// console.log(x * y);
+
+// // /
+// console.log(x / y);
+
+// // **
+// console.log(x ** y);
+
+// // x ++
+// // x++;
+// console.log(x);
+
+// // x--
+// // x--;
+// console.log(x);
+
+// // % ==> mod
+// console.log(x % y);
+
+// ---------------- Second -----------------
+// Assignment Operators
+// let x2 = 5;
+// let y2 = 3;
+
+// // x2 += 5;
+// // console.log(x);
+
+// // y2 -= 5;
+// // console.log(y);
+
+// // x2 *= 2;
+// // console.log(x);
+
+// // x2 **= 3;
+// // console.log(x);
+
+// // x2 %= y;
+// // console.log(x);
+
+// ---------------- Third -----------------
+// Comparison Operators
+// const x = 5;
+// const y = 3;
+// const z = "5";
+
+// 1 ==> x == y
+// console.log(x == y);
+
+// 2 ==> x == z
+// console.log(x == z);
+
+// 3 ==> x != y
+// console.log(x != y);
+
+// 4 ==> x === z
+// console.log(x === z);
+
+// 5 ==> x !=== y
+// console.log(x !== y);
+
+// 6 ==> x > y
+// console.log(x > y);
+
+// 7 ==> x < y
+// console.log(x < y);
+
+// 8 ==> x >= y
+// console.log(x >= y);
+
+// 9==> x <= y
+// console.log(x <= y);
+
+// ---------------- Fourth -----------------
+// Logical Operators
+// const x = 5;
+// const y = 3;
+// const z = "5";
+
+// // Logical Operators ==> and:
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+// // Logical Operators ==> or:
+
+// console.log(true || true);
+// console.log(false || true);
+// console.log(true || false);
+// console.log(false || false);
+
+// // Logical Operators ==> not:
+
+// console.log(!true);
+// console.log(!false);
+
+// ---------------- Fifth -----------------
+// // String Operators
+// const firstName = "habib";
+// const lastName = "Atashbahar";
+// // const finalText = firstName + " " + lastName;
+// const finalText = "Hi, I am " + firstName + " " + lastName;
+// console.log(finalText);
+
+// ---------------- Sixth -----------------
+// Unary Operators
+// const value = "5";
+// const x = 5;
+
+// console.log(+value);
+// console.log(-x);
+
+// ---------------- Seventh -----------------
+// Nullish Coalescing Operators
+// const foo = null ?? "default string";
+// console.log(foo);
+// // Expected output: "default string"
+
+// const baz = 0 ?? 42;
+// console.log(baz);
+// // Expected output: 0
+
+// ---------------- Eighth -----------------
+// Ternary Operators
+// 1. true ? "yes" : "no" ==> "yes"
+// 2. false ? "yes" : "no" ==> "no"
+// const x = 5;
+// const y = 6;
+
+// console.log(x > y ? "yes" : "no");
+// console.log(x < y ? "yes" : "no");
+
+// ---------------- Ninth -----------------
+// Falsy and Truthy Values
+
+// Falsy
+// 0 - "" - null - undefined - false
+// Without this all values will be Truthy
+
+// console.log(0 ? "Truthy" : "Falsy");
+// console.log("str" ? "Truthy" : "Falsy");
